c927d9ec542df7cba0e6b7eb2c4d171d9a1de008
elulajoseph <elula.joseph@gmail.com>
2023-11-21 01:54:47 -0800

    first commit

---
+++ elula.py
@@ -0,0 +1,10 @@
+import turtle as t
+
+t.pen()
+win = t.Screen()
+win.setup(width=800,height=450)
+win.bgpic('jen.gif')
+
+name=t.textinput("Enter Your Name" , "What is your name?")
+name=t.textinput("Enter Your Age" , "What is your age?")
+name=t.textinput("Enter Your Address" , "What is your address?")
